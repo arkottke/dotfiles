bfc4cd8595dd97e052b9714733153a48e3b9cc6b
Albert Kottke <albert.kottke@gmail.com>
2019-01-04 15:34:36 -0800

    Initial commit.

---
+++ fish/config.fish
@@ -0,0 +1,23 @@
+set fish_greeting
+
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \co forward-char
+    end
+end
+
+fish_vi_key_bindings
+
+# set PATH /home/albert/.local/bin $PATH
+set --universal fish_user_paths $fish_user_paths /home/albert/.local/bin
+
+# Always start tmux
+if status is-interactive 
+and not set -q TMUX
+    exec tmux
+end
+
+function ko
+    kde-open5 $argv
+end
+
