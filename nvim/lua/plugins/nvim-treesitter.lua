7e23299188aa4df0d4d7bbe16617717822543b41
Albert Kottke <albert.kottke@gmail.com>
2022-05-29 13:53:58 -0700

    Disable TS for latex.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -20,7 +20,6 @@ require('nvim-treesitter.configs').setup {
     "json",
     "json5",
     "julia",
-    "latex",
     "lua",
     "make",
     "markdown",
@@ -37,6 +36,7 @@ require('nvim-treesitter.configs').setup {
 
   highlight = {
     enable = true,
+    disable = { "latex" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same
     -- time. Set this to `true` if you depend on 'syntax' being enabled (like
