bbde569e85fc74066ca76996738423eb0cc24681
Albert Kottke <albert.kottke@gmail.com>
2022-07-29 14:31:05 -0700

    Setup packer if it doesn't exist.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,22 @@
+-- Install packer on new copies
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+  -- My plugins here
+  require('plugins/packer')
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+
 require('settings')
 require('keymaps')
 require('plugins/aerial')
@@ -14,3 +33,4 @@ require('plugins/packer')
 require('plugins/tabnine')
 require('plugins/telescope')
 require('plugins/tmux-navigation')
+
