580b02f22631d6a39e70f87e6ef6040565c40756
Albert Kottke <albert.kottke@gmail.com>
2022-10-07 10:33:06 -0700

    Add boot strapping.

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -11,12 +11,22 @@
 
 
 local cmd = vim.cmd
-cmd [[packadd packer.nvim]]
 
-local packer = require 'packer'
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Add packages
-return packer.startup(function()
+return require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- packer can manage itself
 
   -- telescope
@@ -148,4 +158,10 @@ return packer.startup(function()
 
   -- Highlighting for kitty config
   use { 'fladson/vim-kitty' }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
