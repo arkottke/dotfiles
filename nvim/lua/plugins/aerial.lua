686eca395830420344083d5aa4aea0737044e14a
Albert Kottke <albert.kottke@gmail.com>
2022-12-14 13:11:34 -0800

    Remove on_attach as no longer needed.

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -1,12 +1,6 @@
 
 require('aerial').setup({
-  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-  on_attach = function(bufnr)
-    -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-    -- Jump up the tree with '[[' or ']]'
-    vim.keymap.set('n', '[[', '<cmd>AerialPrevUp<CR>', {buffer = bufnr})
-    vim.keymap.set('n', ']]', '<cmd>AerialNextUp<CR>', {buffer = bufnr})
-  end
+    -- Priority list of preferred backends for aerial.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  backends = { "treesitter", "lsp", "markdown", "man" },
 })
