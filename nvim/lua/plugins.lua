01b35e8ec9e7bd75672f0dcd329d7de9f32f40f9
Albert Kottke <albert.kottke@gmail.com>
2023-11-16 15:17:24 -0800

    Removed duplicated.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -359,13 +359,15 @@ return {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPost",
-		opts = {
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-		},
+        main = "ibl",
+        opts = {},
+		-- event = "BufReadPost",
+		-- opts = {
+		-- 	char = "│",
+		-- 	filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+		-- 	show_trailing_blankline_indent = false,
+		-- 	show_current_context = false,
+		-- },
 	},
 	{
 		"alexghergh/nvim-tmux-navigation",
@@ -483,25 +485,6 @@ return {
 		end,
 	},
 	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = "BufReadPre",
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		config = function(_, opts)
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-			require("mini.indentscope").setup(opts)
-		end,
-	},
-	{
 		"folke/lazy.nvim",
 		version = false,
 		opts = {
@@ -559,16 +542,6 @@ return {
 			{ "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
 			{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
 			{ "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
-		},
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPost",
-		opts = {
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
 		},
 	},
 	{
