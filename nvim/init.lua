eb2541999e667844139e2dedd84a5e09a7b3203e
Albert Kottke <albert.kottke@gmail.com>
2024-07-01 11:59:38 -0700

    Reformat.

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,14 +8,14 @@ vim.g.loaded_netrwPlugin = 1
 -- bootstrap lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,3 +25,6 @@ require("lazy").setup("plugins")
 require("options")
 require("keymap")
 require("autocmds")
+
+-- Set the pyenv version
+vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match('(%S+)%s+%(.-%)')
